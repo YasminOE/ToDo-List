305a4787d114e88b4c801f7a3fa516f5d9d374a7
Yasmin Elmahdi <elmahdiyasmin@.com>
2023-08-30 14:55:54 +0300

    Split environment setup

--- webpack.common.js
+++ webpack.common.js
@@ -2,14 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-
   entry: './src/index.js',
-  output: {
-    filename: '[name].[hash].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
+
   plugins: [
     new HtmlWebpackPlugin({
 			template: './src/indexSrc.html'
