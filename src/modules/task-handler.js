1f4c549e52c467b13c287a7ab4525ea359c57eb1
Yasmin Elmahdi <elmahdiyasmin@.com>
2023-08-31 02:01:37 +0300

    change handletask logic

--- src/modules/task-handler.js
+++ src/modules/task-handler.js
@@ -1,51 +1,48 @@
 // import date library for due date
 
 // Task Constructor
-class Task {
-  constructor(taskName, description, dueDate, priority, type) {
-    const priorityTypes = ['priorityOne', 'priorityTwo', 'priorityThree', 'priorityFour'];
-    const taskType = ['inbox', 'home', 'projects'];
+function Task(taskName, description, dueDate, priority, type) {
+  const priorityTypes = ['priorityOne', 'priorityTwo', 'priorityThree', 'priorityFour'];
+  const taskType = ['inbox', 'home', 'projects'];
 
-    this.taskName = taskName;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priorityTypes[priority];
-    this.type = taskType[type]; 
-  }
-
-  setName(taskName) {
-    this.taskName = taskName;
-  }
-
-  setDescription(description) {
-    this.description = description;
-  }
-
-  setDueDate(dueDate) {
-    this.dueDate = dueDate;
-  }
-
-  setPriority(priority) {
-    this.priority = priority;
-  }
-
-  setTaskType(type) {
-    this.type = type;
-  }
-
-
+  this.taskName = taskName;
+  this.description = description;
+  this.dueDate = dueDate;
+  this.priority = priorityTypes[priority];
+  this.type = taskType[type];
 }
 
+class TaskManager {
+  constructor() {
+    this.tasks = [];
+  }
 
+  addTask(task) {
+    this.tasks.push(task);
+  }
 
+  removeTask(taskName) {
+    const index = this.tasks.findIndex(task => task.taskName === taskName);
 
+    if (index !== -1) {
+      const removedTask = this.tasks.splice(index, 1);
+      console.log(`${removedTask[0].taskName} has been removed`);
+    }
+  }
+}
 
 // test
 
-export default function  handleTask(){
-  const tasks = [
-    new Task('add task test', 'add test', 'test', '1','2'),
-    new Task('add task test', 'add test', 'test', '4','0'),
-  ]
-  tasks.forEach(task => console.log(task));
+export default function handleTask() {
+  const taskManager = new TaskManager();
+
+  taskManager.addTask(new Task('add task test to be removed', 'add test', 'test', '1', '2'));
+  taskManager.addTask(new Task('add task test', 'add test', 'test', '4', '0'));
+  taskManager.addTask(new Task('add another task test', 'add test', 'test', '4', '0'));
+
+  taskManager.tasks.forEach(task => console.log(task));
+
+  taskManager.removeTask('add task test');
+
+  console.log(taskManager.tasks);
 }
