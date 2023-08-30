3cfebd264b5e3125cdbdf8cb2c81ae539c9f275b
Yasmin Elmahdi <elmahdiyasmin@.com>
2023-08-30 14:06:12 +0300

    Setup webpack part 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,44 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+  
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+			template: './src/index.html'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+         test: /\.html$/,
+         use: ["html-loader"],
+     },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: {
+           loader: 'file-loader',
+           options: {
+             name:"[name].[hash].[ext]",
+             outputPath: "imgs"
+           }
+         },
+      },
+     {
+       test: /\.(woff|woff2|eot|ttf|otf)$/i,
+       type: 'asset/resource',
+     },
+    ],
   },
 };
 
