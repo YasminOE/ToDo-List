dadf857e01b8812cd8ca0429cb89227dd3d089a1
Yasmin Elmahdi <elmahdiyasmin@.com>
2023-08-30 14:08:53 +0300

    Update index.html name in src folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  
+
   entry: './src/index.js',
   output: {
     filename: 'main.js',
@@ -11,7 +11,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-			template: './src/index.html'
+			template: './src/indexSrc.html'
     }),
   ],
   module: {
