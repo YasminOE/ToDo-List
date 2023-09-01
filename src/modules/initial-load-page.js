435f0a1978c2e841945a1fcb02e95843b346104d
Yasmin Elmahdi <elmahdiyasmin@.com>
2023-09-02 02:59:43 +0300

    Update initial-load-page: - Create nav function

--- src/modules/initial-load-page.js
+++ src/modules/initial-load-page.js
@@ -74,8 +74,49 @@ function createHeader() {
 function createNav(){
   const nav = document.createElement('div');
   nav.setAttribute("id", "navbar");
-
   
+  const dateViews = document.createElement('ul');
+  dateViews.setAttribute('id','date-views');
+  nav.appendChild(dateViews);
+
+  const inboxLi =document.createElement('li');
+  inboxLi.setAttribute('id','inbox-view');
+  dateViews.appendChild(inboxLi);
+  inboxLi.innerHTML = '<span class="material-symbols-outlined">inbox</span>Inbox';
+
+  const todayLi =document.createElement('li');
+  todayLi.setAttribute('id','today-view');
+  dateViews.appendChild(todayLi);
+  todayLi.innerHTML = '<span class="material-symbols-outlined">event</span>Today';
+
+  const upcomingLi =document.createElement('li');
+  upcomingLi.setAttribute('id','upcoming-view');
+  dateViews.appendChild(upcomingLi);
+  upcomingLi.innerHTML = '<span class="material-symbols-outlined">calendar_month</span>Upcoming';
+
+
+  const projectViews = document.createElement('ul');
+  projectViews.setAttribute('id','project-views');
+  nav.appendChild(projectViews);
+
+  const homeProjectsLi =document.createElement('li');
+  homeProjectsLi.setAttribute('id','home');
+  projectViews.appendChild(homeProjectsLi);
+  homeProjectsLi.innerHTML = 'Home 🏡';
+
+  const workProjectsLi =document.createElement('li');
+  workProjectsLi.setAttribute('id','work');
+  projectViews.appendChild(workProjectsLi);
+  workProjectsLi.innerHTML = 'My work 🎯';
+
+  const soonViews = document.createElement('ul');
+  soonViews.setAttribute('id','soon');
+  nav.appendChild(soonViews);
+  soonViews.innerHTML = '<li><span>soon</span>Add team workspace</li>'
+
+
+  return nav;
+
 }
 
 export default function loadPage() {
@@ -83,4 +124,7 @@ export default function loadPage() {
 
   const header = createHeader();
   content.appendChild(header);
+
+  const nav = createNav();
+  content.appendChild(nav);
 }
