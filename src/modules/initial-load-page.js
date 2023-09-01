63fb7ba78a56081dba03c348c383315a048d0417
Yasmin Elmahdi <elmahdiyasmin@.com>
2023-09-01 21:25:12 +0300

    Change images icons to google matrial icons, profile pic remains with the issue

--- src/modules/initial-load-page.js
+++ src/modules/initial-load-page.js
@@ -7,17 +7,75 @@ function createHeader() {
   header.appendChild(inboxSec);
 
   const homeLi = document.createElement('li');
-  const homeIcon = document.createElement('img');
-  homeIcon.src = '../icons/home.svg';
-  homeIcon.alt = 'Home Icon';
+  const homeIcon = document.createElement('span');
+  homeIcon.classList.add('material-symbols-outlined');
+  homeIcon.innerHTML = 'home';
+  // const homeIcon = document.createElement('img');
+  // homeIcon.src = '../icons/home.svg';
+  // homeIcon.alt = 'Home Icon';
   homeLi.appendChild(homeIcon);
   inboxSec.appendChild(homeLi);
 
   const searchLi = document.createElement('li');
-  searchLi.innerHTML = '<img src="icons/search.svg" alt=""><input type="text" value="" placeholder="Search">';
+  const searchIcon = document.createElement('span');
+  searchIcon.classList.add('material-symbols-outlined');
+  searchIcon.innerHTML = 'search';
+  searchLi.appendChild(searchIcon);
+
+  const searchInput = document.createElement('input');
+  searchInput.type = 'text';
+  searchInput.value = '';
+  searchInput.placeholder = 'Search';
+
+  searchLi.appendChild(searchInput);
   inboxSec.appendChild(searchLi);
 
+
+  const logoSec = document.createElement('ul');
+  logoSec.setAttribute('id', 'logo');
+  header.appendChild(logoSec);
+
+  const logoIcon = document.createElement('img');
+  logoIcon.src = './icons/logo1.png';
+  logoIcon.alt = 'Logo Icon';
+  logoSec.appendChild(logoIcon);
+
+
+  const accountSec = document.createElement('ul');
+  accountSec.setAttribute('id', 'account-info');
+  header.appendChild(accountSec);
+
+  const addTaskLi = document.createElement('li');
+  addTaskLi.setAttribute('id', 'add-task');
+  const addTaskIcon = document.createElement('span');
+  addTaskIcon.classList.add('material-symbols-outlined');
+  addTaskIcon.innerHTML = 'add';
+  addTaskLi.appendChild(addTaskIcon);
+  accountSec.appendChild(addTaskLi);
+
+  const notificationLi = document.createElement('li');
+  notificationLi.setAttribute('id', 'notification');
+  const notificationIcon = document.createElement('span');
+  notificationIcon.classList.add('material-symbols-outlined');
+  notificationIcon.innerHTML = 'notifications';
+  notificationLi.appendChild(notificationIcon);
+  accountSec.appendChild(notificationLi);
+
+  const profilePicLi = document.createElement('li');
+  profilePicLi.setAttribute('id', 'profile-pic');
+  const profileIcon = document.createElement('img');
+  profileIcon.src = './icons/profile-pic.png';
+  profileIcon.alt = 'profile Icon';
+  profilePicLi.appendChild(profileIcon);
+  accountSec.appendChild(profilePicLi);
   return header;
+}
+
+function createNav(){
+  const nav = document.createElement('div');
+  nav.setAttribute("id", "navbar");
+
+  
 }
 
 export default function loadPage() {
--- webpack.config.js
+++ webpack.config.js
@@ -25,9 +25,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-        // generator: {
-        //   filename: 'icons/[name][ext]',
-        // },
+        generator: {
+          filename: './icons/[name][ext]',
+        },
         // use: {
         //    loader: 'file-loader',
         //    options: {
